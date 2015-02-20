6e3156b708f348746a9883e342622a7fe60e5435
thitiporn0643 <teammysmile@gmail.com>
2015-02-20 16:16:14 +0700

    //temm
    ugyyg

--- app/src/main/java/com/example/administrator/sudoku1/MainActivity3.java
+++ app/src/main/java/com/example/administrator/sudoku1/MainActivity3.java
@@ -10,16 +10,22 @@ import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.PopupMenu;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.CharArrayWriter;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Timer;
 import java.util.TimerTask;
+import android.view.ContextMenu;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
 
 
 
@@ -28,6 +34,8 @@ public class MainActivity3 extends ActionBarActivity {
     Timer timer = new Timer();
     TimerTask timetask;
     long starttime = 0L;
+    int seconds = 0;
+    int pause=0;
     static int [][] model;
 
     @Override
@@ -36,64 +44,78 @@ public class MainActivity3 extends ActionBarActivity {
         setContentView(R.layout.activity_main_activity3);
         Intent model = this.getIntent();
         int[] input = model.getIntArrayExtra("value");
-       fromPuzzleString(input);
+        fromPuzzleString(input);
+        //Button btn = (Button) findViewById(R.id.pauseclick);
+       // registerForContextMenu(btn);
         starttime=SystemClock.uptimeMillis();
-      doTask();
+        //handler.postDelayed(updateTimerThread, 0);
+         doTask();
 
     }
-    public void doTask(){
-               timetask = new TimerTask() {
+
+    public void doTask() {
+
+        timetask = new TimerTask() {
+
                 public void run() {
-                    handler.post(new Runnable() {
-                         public void run() {
-//                            Calendar c = Calendar.getInstance();
-//
-//                        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//
-//                        String formattedDate = df.format(c.getTime());
+                handler.post(new Runnable() {
+                    public void run() {
+                        if(pause==0) {
+                            seconds++;
+                        }
 
-                             long millis= SystemClock.uptimeMillis()-starttime;
-                             int seconds = (int) (millis / 1000);
+                        //long millis = SystemClock.uptimeMillis() - starttime;
+                        //int seconds = (int) (millis / 1000);
 
-                              int   minutes = seconds / 60;
-                             seconds     = seconds % 60;
+                        int minutes = seconds / 60;
+                        seconds = seconds % 60;
 
 
                         TextView result = (TextView) findViewById(R.id.txtResult);
-                             result.setText(String.format("%02d:%02d", minutes, seconds));
-
-//                        result.setText();
+                        result.setText(String.format("%02d:%02d", minutes, seconds));
 
                     }
 
                 });
 
-            }};
+            }
+        };
 
         timer.schedule(timetask, 0, 1000); // Every 1 second
 
-    }
-
-    public void stopTask(){
-
-        if(timetask != null)
-
-        {
-
-            timetask.cancel();
-
-            TextView result = (TextView) findViewById(R.id.txtResult);
-
-            result.setText("TimeTask stop.");
-
-        }
 
     }
+   public void PauseonClicked(View view){
 
 
+       if(pause ==0) {
+           pause = 1;
+       }
+
+   }
+   // @Override
+//    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo)
+//    {
+//        super.onCreateContextMenu(menu, v, menuInfo);
+//        menu.setHeaderTitle("Select The Action");
+//        menu.add(0, v.getId(), 0, "Call");//groupId, itemId, order, title
+//        menu.add(0, v.getId(), 0, "SMS");
+//    }
+//    @Override
+//    public boolean onContextItemSelected(MenuItem item){
+//        if(item.getTitle()=="Call"){
+//            Toast.makeText(getApplicationContext(),"calling code",Toast.LENGTH_LONG).show();
+//        }
+//        else if(item.getTitle()=="SMS"){
+//            Toast.makeText(getApplicationContext(),"sending sms code",Toast.LENGTH_LONG).show();
+//        }else{
+//            return false;
+//        }
+//        return true;
+//    }
 
 
-    @Override
+  //  @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
